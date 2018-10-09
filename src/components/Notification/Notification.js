9c3e51f15d68aee10587fda12fd1c2107ed7d143
Simeon Petkov Stoykov <simeon_stoykov@mail.bg>
2018-10-09 01:39:10 +0100

    style changes

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -4,7 +4,6 @@ import './Notification.css';
 export default class Notification extends Component {
     constructor(props) {
         super(props);
-
         this.handlePromoNotificationCLick = this.handlePromoNotificationCLick.bind(this);
     }
 
@@ -16,36 +15,36 @@ export default class Notification extends Component {
         let notification = null;
 
         switch (this.props.data.type) {
-            // case 'text': {
-            //     let { key, title, text } = this.props.data;
-            //     notification =
-            //         <div key={key} className="notification-wrapper">
-            //             <h4 className="text-title">{title}</h4>
-            //             <p>{text}</p>
-            //             <hr />
-            //         </div>;
-            // }
-            //     break;
+            case 'text': {
+                let { key, title, text } = this.props.data;
+                notification =
+                    <div key={key} className="notification-wrapper">
+                        <h4 className="text-title">{title}</h4>
+                        <p>{text}</p>
+                    </div>;
+            }
+                break;
             case 'promotion': {
                 let { title, image, link } = this.props.data;
 
                 notification =
                     <div key={this.props.notificationIndex} className="notification-wrapper cursor-pointer" onClick={this.handlePromoNotificationCLick.bind(this, link)}>
                         <h4 className="text-title">{title}</h4>
-                        <img src={image} alt="Promotion Image" className="loading promo-img" />
-                        <hr />
+                        <img src={image} alt="" title="Promotion" className="loading promo-img" />
                     </div>;
             }
                 break;
-            // case 'bonus': {
-            //     let { key, title, requirement } = this.props.data;
-            //     notification =
-            //         <div key={key} className="notification-wrapper">
-            //             {title} <br /> {requirement}
-            //             <hr />
-            //         </div>;
-            // }
-            //     break;
+            case 'bonus': {
+                let { key, title, requirement } = this.props.data;
+                notification =
+                    <div key={key} className="notification-wrapper">
+                        <h4 className="text-title">{title} - Bonus</h4>
+                        <p>{requirement}</p>
+                    </div>;
+            }
+                break;
+            default:
+                break;
         }
 
         return notification;
