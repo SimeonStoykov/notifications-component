3140d4ed11638c5c063038edc071ce0ee3dad1fc
Simeon Stoykov <stoykosi@y02091.local>
2018-10-09 17:10:33 +0100

    uncommented api code

--- src/api.js
+++ src/api.js
@@ -16,42 +16,42 @@ chaos.register(faker => {
     };
 }, 'low');
 
-// chaos.register(faker => {
-//     return {
-//         id: faker.random.number({
-//             min: 21,
-//             max: 40
-//         }),
-//         type: 'bonus',
-//         title: faker.lorem.sentence(),
-//         requirement: faker.lorem.sentence(),
-//         expires: faker.random.arrayElement([faker.random.number({ min: MIN_EXPIRE, max: MAX_EXPIRE }), null])
-//     };
-// }, 'low');
+chaos.register(faker => {
+    return {
+        id: faker.random.number({
+            min: 21,
+            max: 40
+        }),
+        type: 'bonus',
+        title: faker.lorem.sentence(),
+        requirement: faker.lorem.sentence(),
+        expires: faker.random.arrayElement([faker.random.number({ min: MIN_EXPIRE, max: MAX_EXPIRE }), null])
+    };
+}, 'low');
 
-// chaos.register(faker => {
-//     return {
-//         id: faker.random.number({
-//             min: 41,
-//             max: 60
-//         }),
-//         type: 'promotion',
-//         image: faker.image.image(),
-//         title: faker.lorem.sentence(),
-//         link: faker.internet.url(),
-//         expires: faker.random.arrayElement([faker.random.number({ min: MIN_EXPIRE, max: MAX_EXPIRE }), null])
-//     };
-// }, 'low');
+chaos.register(faker => {
+    return {
+        id: faker.random.number({
+            min: 41,
+            max: 60
+        }),
+        type: 'promotion',
+        image: faker.image.image(),
+        title: faker.lorem.sentence(),
+        link: faker.internet.url(),
+        expires: faker.random.arrayElement([faker.random.number({ min: MIN_EXPIRE, max: MAX_EXPIRE }), null])
+    };
+}, 'low');
 
-// chaos.register(faker => {
-//     return {
-//         id: faker.random.number({
-//             min: 1,
-//             max: 3
-//         }),
-//         type: 'remove-notification'
-//     };
-// }, 'low');
+chaos.register(faker => {
+    return {
+        id: faker.random.number({
+            min: 1,
+            max: 3
+        }),
+        type: 'remove-notification'
+    };
+}, 'low');
 
 chaos.listen({
     delay: 5000
